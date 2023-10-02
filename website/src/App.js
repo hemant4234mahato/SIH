06f8499c19510240311be42585f398d2c1af627d
Kumarsab11 <138751580+Kumarsab11@users.noreply.github.com>
2023-10-02 15:41:22 +0530

    nav

--- website/src/App.js
+++ website/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./Components/navbar";
+import { About } from "./Pages/About";
+import { Contact } from "./Pages/Contact";
+import { Services } from "./Pages/Services";
+import { Home } from "./Pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/Service" element={<Services />} />
+      </Routes>
     </div>
   );
 }
---
+++ website/src/Pages/Services.jsx
@@ -0,0 +1,156 @@
+import React from "react";
+
+export const Services = () => {
+  return (
+    <div>
+      <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet nostrum
+        reprehenderit ipsa odio consequuntur voluptate perferendis corporis
+        temporibus blanditiis expedita, maiores aut libero. Amet ab fugit
+        voluptatibus possimus earum dolore?
+        <br />
+      </div>
+      <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet nostrum
+        reprehenderit ipsa odio consequuntur voluptate perferendis corporis
+        temporibus blanditiis expedita, maiores aut libero. Amet ab fugit
+        voluptatibus possimus earum dolore?
+        <br />
+      </div>
+      <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet nostrum
+        reprehenderit ipsa odio consequuntur voluptate perferendis corporis
+        temporibus blanditiis expedita, maiores aut libero. Amet ab fugit
+        voluptatibus possimus earum dolore?
+        <br />
+      </div>
+      <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet nostrum
+        reprehenderit ipsa odio consequuntur voluptate perferendis corporis
+        temporibus blanditiis expedita, maiores aut libero. Amet ab fugit
+        voluptatibus possimus earum dolore?
+        <br />
+      </div>
+      <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet nostrum
+        reprehenderit ipsa odio consequuntur voluptate perferendis corporis
+        temporibus blanditiis expedita, maiores aut libero. Amet ab fugit
+        voluptatibus possimus earum dolore?
+        <br />
+      </div>
+      <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet nostrum
+        reprehenderit ipsa odio consequuntur voluptate perferendis corporis
+        temporibus blanditiis expedita, maiores aut libero. Amet ab fugit
+        voluptatibus possimus earum dolore?
+        <br />
+      </div>
+      <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet nostrum
+        reprehenderit ipsa odio consequuntur voluptate perferendis corporis
+        temporibus blanditiis expedita, maiores aut libero. Amet ab fugit
+        voluptatibus possimus earum dolore?
+        <br />
+      </div>
+      <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet nostrum
+        reprehenderit ipsa odio consequuntur voluptate perferendis corporis
+        temporibus blanditiis expedita, maiores aut libero. Amet ab fugit
+        voluptatibus possimus earum dolore?
+        <br />
+      </div>
+      <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet nostrum
+        reprehenderit ipsa odio consequuntur voluptate perferendis corporis
+        temporibus blanditiis expedita, maiores aut libero. Amet ab fugit
+        voluptatibus possimus earum dolore?
+        <br />
+      </div>
+      <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet nostrum
+        reprehenderit ipsa odio consequuntur voluptate perferendis corporis
+        temporibus blanditiis expedita, maiores aut libero. Amet ab fugit
+        voluptatibus possimus earum dolore?
+        <br />
+      </div>
+      <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet nostrum
+        reprehenderit ipsa odio consequuntur voluptate perferendis corporis
+        temporibus blanditiis expedita, maiores aut libero. Amet ab fugit
+        voluptatibus possimus earum dolore?
+        <br />
+      </div>
+      <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet nostrum
+        reprehenderit ipsa odio consequuntur voluptate perferendis corporis
+        temporibus blanditiis expedita, maiores aut libero. Amet ab fugit
+        voluptatibus possimus earum dolore?
+        <br />
+      </div>
+      <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet nostrum
+        reprehenderit ipsa odio consequuntur voluptate perferendis corporis
+        temporibus blanditiis expedita, maiores aut libero. Amet ab fugit
+        voluptatibus possimus earum dolore?
+        <br />
+      </div>
+      <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet nostrum
+        reprehenderit ipsa odio consequuntur voluptate perferendis corporis
+        temporibus blanditiis expedita, maiores aut libero. Amet ab fugit
+        voluptatibus possimus earum dolore?
+        <br />
+      </div>
+      <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet nostrum
+        reprehenderit ipsa odio consequuntur voluptate perferendis corporis
+        temporibus blanditiis expedita, maiores aut libero. Amet ab fugit
+        voluptatibus possimus earum dolore?
+        <br />
+      </div>
+      <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet nostrum
+        reprehenderit ipsa odio consequuntur voluptate perferendis corporis
+        temporibus blanditiis expedita, maiores aut libero. Amet ab fugit
+        voluptatibus possimus earum dolore?
+        <br />
+      </div>
+      <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet nostrum
+        reprehenderit ipsa odio consequuntur voluptate perferendis corporis
+        temporibus blanditiis expedita, maiores aut libero. Amet ab fugit
+        voluptatibus possimus earum dolore?
+        <br />
+      </div>
+      <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet nostrum
+        reprehenderit ipsa odio consequuntur voluptate perferendis corporis
+        temporibus blanditiis expedita, maiores aut libero. Amet ab fugit
+        voluptatibus possimus earum dolore?
+        <br />
+      </div>
+      <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet nostrum
+        reprehenderit ipsa odio consequuntur voluptate perferendis corporis
+        temporibus blanditiis expedita, maiores aut libero. Amet ab fugit
+        voluptatibus possimus earum dolore?
+        <br />
+      </div>
+      <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet nostrum
+        reprehenderit ipsa odio consequuntur voluptate perferendis corporis
+        temporibus blanditiis expedita, maiores aut libero. Amet ab fugit
+        voluptatibus possimus earum dolore?
+        <br />
+      </div>
+      <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet nostrum
+        reprehenderit ipsa odio consequuntur voluptate perferendis corporis
+        temporibus blanditiis expedita, maiores aut libero. Amet ab fugit
+        voluptatibus possimus earum dolore?
+        <br />
+      </div>
+    
+    </div>
+  );
+};
