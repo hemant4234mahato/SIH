58980a4ffd8ee2626b65b2940791fd8e919a8efa
Kumarsab11 <138751580+Kumarsab11@users.noreply.github.com>
2023-10-09 19:30:07 +0530

    null

--- website/src/App.js
+++ website/src/App.js
@@ -1,22 +1,31 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import Navbar from "./Components/navbar";
-import { About } from "./Pages/About";
-import { Contact } from "./Pages/Contact";
-import { Services } from "./Pages/Services";
-import { Home } from "./Pages/Home";
-
+import Header from "./components/common/header/Header";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import About from "./components/about/About";
+import CourseHome from "./components/allcourses/CourseHome";
+import Team from "./components/team/Team";
+import Pricing from "./components/pricing/Pricing";
+import Blog from "./components/blog/Blog";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/common/footer/Footer";
+import Home from "./components/home/Home";
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/Service" element={<Services />} />
-      </Routes>
-    </div>
+    <>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/courses" component={CourseHome} />
+          <Route exact path="/team" component={Team} />
+          <Route exact path="/pricing" component={Pricing} />
+          <Route exact path="/journal" component={Blog} />
+          <Route exact path="/contact" component={Contact} />
+        </Switch>
+        <Footer />
+      </Router>
+    </>
   );
 }
 
